98a6c5146d37774f26022c753ad14eb6f79c81d1
gonga0 <alskdjfhg1346@naver.com>
2024-05-15 19:34:39 +0900

    Add : 길이에 따른 연산

--- src/Lv_0/길이에_따른_연산.java
+++ src/Lv_0/길이에_따른_연산.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
+
         if (num_list.length >= 11) {
             for (int i = 0; i < num_list.length; i++) {
                 answer += num_list[i];
